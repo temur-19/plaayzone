9abd491896a644bd05d1be4ee02975db2d0485ca
Temur <temurxasanov320@gmail.com>
2026-06-19 18:00:45 +0500

    a

--- Userregistration/admin.py
+++ Userregistration/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import CustomUser
+from django.contrib.auth.forms import UserCreationForm
 
-@admin.register(CustomUser)
+from .models import CustomUser,Profile
+
+admin.site.register(CustomUser)
+admin.site.register(Profile)
+
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
     list_filter = ('is_staff', 'is_active')
     search_fields = ('username', 'email')
 
 # Register your models here.
+
+
